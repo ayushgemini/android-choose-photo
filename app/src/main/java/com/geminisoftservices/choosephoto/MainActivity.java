83903b8f6d316f60e9f7ab75c5fc0e46ddd71ea8
Ayush Gemini <ayushgemini13@gmail.com>
2020-05-07 21:10:19 +0530

    Update-- MainActivity

--- app/src/main/java/com/geminisoftservices/choosephoto/MainActivity.java
+++ app/src/main/java/com/geminisoftservices/choosephoto/MainActivity.java
@@ -141,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
